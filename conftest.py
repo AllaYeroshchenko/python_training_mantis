9e869d66a7d70be0a17235beafb3ac04bc7310c0
AllaYeroshchenko <43897472+AllaYeroshchenko@users.noreply.github.com>
2019-02-13 07:26:17 -0800

    Task 25

---
+++ conftest.py
@@ -0,0 +1,55 @@
+from fixture.application import Application
+import pytest
+import json
+import os.path
+import importlib
+from fixture.db import DbFixture
+
+fixture = None
+target = None
+
+def load_config(file):
+    global target
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+        with open(config_file) as f:
+            target=json.load(f)
+    return target
+
+
+@pytest.fixture
+def app(request):
+    global fixture
+    global target
+    browser = request.config.getoption("--browser")
+    web_config = load_config(request.config.getoption('--target'))["web"]
+    web_login = load_config(request.config.getoption('--target'))["webadmin"]
+    if fixture is None or not fixture.is_valid():
+        fixture=Application(browser=browser, base_url=web_config['baseUrl'])
+    fixture.session.ensure_login(username=web_login['username'], password=web_login['password'])
+    return fixture
+
+@pytest.fixture(scope="session")
+def db(request):
+    db_config = load_config(request.config.getoption('--target'))["db"]
+    dbfixture = DbFixture(host=db_config["host"], name=db_config["name"], user=db_config["user"], password=db_config["password"])
+    def fin():
+        dbfixture.destroy()
+    request.addfinalizer(fin)
+    return dbfixture
+
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    def fin():
+        fixture.session.ensure_logout()
+        fixture.destroy()
+    request.addfinalizer(fin)
+    return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--target", action="store", default="target.json")
+
